7a4d3f1903c8d746bdd38c4b0cb529c4866645d8
temor01 <134977930+temor01@users.noreply.github.com>
2024-03-07 11:18:14 -0700

    still doing some practices, just added some new javascript code

--- javascript.js
+++ javascript.js
@@ -3,3 +3,13 @@ console.log("Hello, World!");
 
 console.log("so far only containers for header and footer has been created. 04/03/2024")
 
+
+
+const button = document.querySelector("button");
+
+ button.addEventListener("click", updateName);
+
+function updateName() {
+    const name = prompt("Enter a new name");
+    button.textContent = 'Player 1: ${name}';
+}
