536426bb660376a54bfd61cb43fe4c62a5c51099
temor01 <134977930+temor01@users.noreply.github.com>
2024-03-13 23:22:22 -0600

    Update javascript.js
    southpark

--- javascript.js
+++ javascript.js
@@ -3,6 +3,6 @@ const button = document.querySelector("button");
 button.addEventListener("click", updateName);
 
 function updateName() {
-  const name = prompt("Enter a new name");
+  const name = prompt("Enter a new name, douchebag");
   button.textContent = `Player 1: ${name}`;
 }
