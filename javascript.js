ada3f27869db496c0c10afc30b653cb1a9293fa9
temor01 <134977930+temor01@users.noreply.github.com>
2024-03-26 16:14:09 -0600

    Update javascript.js
    addded a greeting for myself

--- javascript.js
+++ javascript.js
@@ -6,3 +6,10 @@ function updateName() {
   const name = prompt("Enter a new name, douchebag");
   button.textContent = `Player 1: ${name}`;
 }
+
+
+let greet = prompt("Hello weary traveller, who night you be? ");
+
+if greet === ('Arsalan' || 'arsalan') {
+  alert("Welcome, enjoy coming!");
+}
